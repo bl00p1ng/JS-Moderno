1cac553428d305907b72417f4c3ab0121221360e
bl00p1ng <blooping@protonmail.com>
2022-05-05 12:18:57 -0500

    019. String Methods - Repeat y Split

---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,13 @@
+const product = 'Monitor de 20 pulgadas ';
+
+// Rdpetir str
+const text = 'en promoción'.repeat(3);
+console.log(text);
+console.log(`${product}${text}!!!`);
+
+// Dividir str
+const activity = 'Estoy aprendiendo JavaScript Moderno';
+console.log(activity.split(' '));
+
+const hobbies = 'Leer, caminar, escuchar música, aprender a programar';
+console.log(hobbies.split(', '));
