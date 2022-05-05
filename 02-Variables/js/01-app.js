d8ca5fb718e114563c0388ee175095db3b8bd32a
bl00p1ng <blooping@protonmail.com>
2022-05-05 10:00:08 -0500

    011.  variables con var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,19 @@
+// Crear e inicializar
+var product = 'Monitor de 24 pulgadas';
+console.log(product);
+
+// Reasignar valor
+product = 'Monitor de 19 pulagadas';
+console.log(product);
+
+// Se pueden inicializar sin ningún valor
+var avaliable;
+avaliable = true;
+
+// Inicializar múltiples variables
+var category = 'Computadoras',
+    brand = 'Asus',
+    rating = 5;
+
+// Nombre con más de una palabra
+var productName;
