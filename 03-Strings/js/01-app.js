242240cba5d3a455b873685e9bdf3d717b8fb775
bl00p1ng <blooping@protonmail.com>
2022-05-05 10:22:40 -0500

    014. Crear Strings en JavaScript

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,7 @@
+const product = "Monitor 24\"";
+const product2 = String('Monitor de 24 pulgadas');
+const product3 = new String('Monitor de 24 pulgadas');
+
+console.log(product);
+console.log(product2);
+console.log(product3);
