a9334a63a69bbb9bcf51c4ac6d2b20380fe80509
bl00p1ng <blooping@protonmail.com>
2022-05-05 14:57:48 -0500

    025. Incrementos o Decrementos

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,23 @@
+// Incremento / decremento
+
+let score = 5;
+
+// INCREMENTO EN 1
+// Se llama primero a puntaje y luego se suma
+score++;
+
+// Se suma primero y luego se asigna a la variable
+++score;
+
+// DECREMENTO EN 1
+score--;
+
+--score;
+
+// INCREMENTO EN MÁS DE 1
+score += 3;
+
+// DECREMENTO EN MÁS DE 1
+score -= 3;
+
+console.log(score);
