bc328003b0c32b482fae95594452102db8e8a491
bl00p1ng <blooping@protonmail.com>
2022-05-05 14:27:13 -0500

    021. Crear Números en JavaScript

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,16 @@
+// Crear números
+const number1 = 20;
+console.log(number1);
+
+const number2 = 2.20;
+console.log(number2);
+
+const number3 = .39825;
+console.log(number3);
+
+const number4 = -20;
+console.log(number4);
+
+const number5 = new Number(80);
+console.log(number5);
+
