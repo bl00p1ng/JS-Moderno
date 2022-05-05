244112a411d0e5320db75227efa839d54b5001fc
bl00p1ng <blooping@protonmail.com>
2022-05-05 15:14:27 -0500

    027. Operador Mayor que y Menor Que

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,10 @@
+const number1 = 20;
+const number2 = '20';
+const number3 = 30;
+
+// Mayor que
+console.log(number1 > number3);
+console.log(number3 > number2);
+
+// Menor que
+console.log(number1 < number3);
