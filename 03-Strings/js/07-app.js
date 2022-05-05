16f6da3c4b652a05009effa24341236b9e63b817
bl00p1ng <blooping@protonmail.com>
2022-05-05 12:27:14 -0500

    020. String Methods - Convertir a Mayusculas o Minusculas

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,13 @@
+const product = 'Monitor de 20 pulgadas';
+
+// Mayuscula
+console.log(product.toUpperCase());
+
+// Minuscula
+console.log(product.toLocaleLowerCase());
+
+// Convertir a str
+price = 350;
+console.log(price);
+console.log(price.toString());
+
