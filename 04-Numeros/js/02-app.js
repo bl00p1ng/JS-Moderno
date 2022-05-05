54d396bd2acdfde39dd4af8c11ff30e32e34db83
bl00p1ng <blooping@protonmail.com>
2022-05-05 14:31:50 -0500

    022. Operaciones en JavaScript

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,23 @@
+// Operaciones básicas
+
+const number1 = 30;
+const number2 = 20;
+
+let result;
+
+// Suma
+result = number1 + number2;
+
+// Resta
+result = number1 - number2;
+
+// Multiplicación
+result = number1 * number2;
+
+// División
+result = number1 / number2;
+
+// Módulo
+result = number1 % number2;
+
+console.log(result);
