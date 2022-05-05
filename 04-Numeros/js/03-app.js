34a9bc152edffdc57b78ae38fc234fd22a111d0b
bl00p1ng <blooping@protonmail.com>
2022-05-05 14:45:10 -0500

    023 El Objeto Math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,32 @@
+let result;
+
+// PI
+result = Math.PI;
+
+// Redondear
+result = Math.floor(2.8);
+result = Math.ceil(2.5);
+result = Math.round(2.6);
+
+// Raíz cuadrada
+result = Math.sqrt(144);
+
+// Valor absoluto
+result = Math.abs(-500);
+
+// Potencia
+result = Math.pow(2, 4);
+
+// Mínimo
+result = Math.min(5, 8, 12, 3, 2, 11);
+
+// Máximo
+result = Math.max(5, 6, 2, 9, 12, 35, 1);
+
+// Aleatorio
+result = Math.random();
+
+// Aleatorio dentro de un rango (0 - 30)
+result = Math.floor(Math.random() * 30);
+
+console.log(result);
