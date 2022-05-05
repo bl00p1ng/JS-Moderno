0e448abf626014764f9c4cc1792f5fdf72cf658c
bl00p1ng <blooping@protonmail.com>
2022-05-05 10:13:25 -0500

    013. variables con const

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,9 @@
+const product = 'Tablet';
+
+// No se puede reasignar
+product = 'Monitor';
+console.log(product);
+
+// Hay que inicializar con un valor
+// const price; -> Incorrecto
+const price = 520;
