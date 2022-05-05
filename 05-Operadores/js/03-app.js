5017609dc05275294d42d7ac2b29ce7b06257382
bl00p1ng <blooping@protonmail.com>
2022-05-05 15:32:54 -0500

    029. Comprar Null y undefined en JavaScript

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,13 @@
+// Undefined
+let number;
+
+console.log(number);
+console.log(typeof number);
+
+// Null
+let number2 = null;
+console.log(number2);
+console.log(typeof number2);
+
+console.log(number == number2); // True
+console.log(number === number2); // False
