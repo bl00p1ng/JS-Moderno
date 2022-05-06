c3f8f6581fcc6ee033b04c098c89228eee6efa9b
bl00p1ng <blooping@protonmail.com>
2022-05-06 18:08:55 -0500

    037 Objetos dentro de Objetos

---
+++ 07-Objetos/js/05-app.js
@@ -0,0 +1,16 @@
+const product = {
+    name: 'Monitor 20 pulgadas',
+    price: 350,
+    isAvailable: true,
+    information: {
+        weight: '1kg',
+        width: '1m'
+    }
+};
+
+console.log(product);
+
+console.log(product.information);
+console.log(product.information.weight);
+console.log(product.information.width);
+
