3fb3a7587ab94ba5012fc00c4549f487fb502679
bl00p1ng <blooping@protonmail.com>
2022-05-06 17:29:11 -0500

    032. Buenas practicas a la hora de evaluar un Boolean

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const isAuthenticated = true;
+
+if (isAuthenticated) {
+    console.log('Si puedes ver Netflix');
+} else {
+    console.log('No puedes ver Netflix :(');
+}
+
+// Operador ternario
+console.log(isAuthenticated ? 'Si esta autenticado' : 'No esta autenticado');
