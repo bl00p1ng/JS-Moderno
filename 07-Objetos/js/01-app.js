dc323e624b9335a8ae2d831f8083073d6551cbd2
bl00p1ng <blooping@protonmail.com>
2022-05-06 17:39:12 -0500

    033 Crear Objetos en JavaScript

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,14 @@
+const name = 'Monitor 20 pulgadas';
+const price = 350;
+const isAvailable = true;
+
+// Un objeto puede agrupar todo lo anterior en una sola variable
+
+// Object Literal
+const product = {
+    name: 'Monitor 20 pulgadas',
+    price: 350,
+    isAvailable: true
+};
+
+console.table(product);
