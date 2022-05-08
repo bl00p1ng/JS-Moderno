4c93f96979bb7315caf012404b01476a2d74030e
bl00p1ng <blooping@protonmail.com>
2022-05-07 19:44:17 -0500

    046 Crear Arrays en JavaScript

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,12 @@
+// Crear Array
+const numbers = [10, 20, 30, 40];
+
+// Otra forma de crear arreglos
+const months = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo');
+
+console.log(numbers);
+console.log(months);
+
+// Un array que contiene datos de todo tipo
+const everything = ['Hola', 10, true, {name: 'Andrés', work: 'Developer'}, null, [1, 2, 3]];
+console.log(everything);
