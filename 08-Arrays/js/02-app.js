826afff6c70dffc1690f8479c806f86565918eb5
bl00p1ng <blooping@protonmail.com>
2022-05-07 20:53:12 -0500

    047 Acceder a los valores de un Array

---
+++ 08-Arrays/js/02-app.js
@@ -0,0 +1,9 @@
+const numbers = [10, 20, 30, 40, 50, [1, 2, 3]];
+console.log(numbers);
+console.table(numbers);
+
+// Acceder al array
+console.log(numbers[2]);
+console.log(numbers[0]);
+console.log(numbers[20]);  // Undefined
+console.log(numbers[5][1]);
