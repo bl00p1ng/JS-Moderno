005e97834e214c9fca2ec42468f2064bfa2ac2ab
bl00p1ng <blooping@protonmail.com>
2022-05-07 21:01:18 -0500

    048 Recorrer un Array

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,11 @@
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+
+console.table(months);
+
+// Longitud del array
+console.log(months.length);
+
+// Recorrer array
+for (let i = 0; i < months.length; i++) {
+    console.log(months[i]);
+}
