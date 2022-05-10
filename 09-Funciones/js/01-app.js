e7be5bcb15d93ea626dbcb27d1e064772b716f57
bl00p1ng <blooping@protonmail.com>
2022-05-09 19:50:21 -0500

    056 Crear Funciones en JS - Function expression y Function Declaration

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,13 @@
+// Function declaration (Declaración de función)
+function sum() {
+    console.log(2 + 2);
+}
+
+sum();
+
+// Function Expression
+const sum2 = function () {
+    console.log(3 + 3);
+};
+
+sum2();
