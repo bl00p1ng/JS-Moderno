dcaa178f8ffacda3c2d50db7d64641781c9b83bc
bl00p1ng <blooping@protonmail.com>
2022-05-11 21:04:04 -0500

    071 Comparando Mayor o Menor que

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,9 @@
+// Operador mayor y menor que
+const money = 500;
+const amountToBuy = 300;
+
+if (money >= amountToBuy) {
+    console.log('Si podemos pagar');
+} else {
+    console.log('Fondos insuficientes');
+}
