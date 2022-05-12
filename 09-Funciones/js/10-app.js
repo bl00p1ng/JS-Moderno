7816462bb05b1ae001037231e68b6f4c5a574238
bl00p1ng <blooping@protonmail.com>
2022-05-11 19:57:05 -0500

    065 Arrow Functions

---
+++ 09-Funciones/js/10-app.js
@@ -0,0 +1,8 @@
+const learning = function() {
+    console.log('Aprendiendo JavaScript');
+};
+learning();
+
+// La anterior función se puede declarar de forma más corta con un arrow function
+const learning2 = () => 'Aprendiendo JavaScript';
+console.log(learning2());
