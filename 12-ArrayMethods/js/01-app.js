9d140f863fddbbb221444e688cbf1c83d252503e
bl00p1ng <blooping@protonmail.com>
2022-05-12 14:18:25 -0500

    086 .some

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -1,10 +1,31 @@
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
-const carrito = [
-    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
-    { nombre: 'Televisión', precio: 100 },
-    { nombre: 'Tablet', precio: 200 },
-    { nombre: 'Audifonos', precio: 300 },
-    { nombre: 'Teclado', precio: 400 },
-    { nombre: 'Celular', precio: 700 },
-]
+const cart = [
+    { name: 'Monitor 27 Pulgadas', precio: 500 },
+    { name: 'Televisión', precio: 100 },
+    { name: 'Tablet', precio: 200 },
+    { name: 'Audifonos', precio: 300 },
+    { name: 'Teclado', precio: 400 },
+    { name: 'Celular', precio: 700 },
+];
+
+// Revisar si existe un valor en un Array
+
+// Forma manual
+// months.forEach(month => {
+//     if(month == 'Enero') {
+//         console.log('Enero si existe');
+//     }
+// });
+
+// Array method
+const result = months.includes('Enero');
+console.log(result);
+
+// Revisar si existe un valor en un Array de objetos -> some()
+const exist = cart.some(product => product.name === 'celular');
+console.log(exist);
+
+// some() en arrays normales
+const exist2 = months.some(mes => mes === 'Febrero');
+console.log(exist2);
