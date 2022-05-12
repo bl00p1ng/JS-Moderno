18a8232449f81910c4b11d223ca2967ca0b54055
bl00p1ng <blooping@protonmail.com>
2022-05-12 16:06:28 -0500

    091 .every

---
+++ 12-ArrayMethods/js/06-app.js
@@ -0,0 +1,11 @@
+const cart = [
+    { name: 'Monitor 27 Pulgadas', price: 500 },
+    { name: 'Televisión', price: 100 },
+    { name: 'Tablet', price: 200 },
+    { name: 'Audifonos', price: 300 },
+    { name: 'Teclado', price: 400 },
+    { name: 'Celular', price: 700 },
+];
+
+const result = cart.every(product => product.price < 1000);
+console.log(result);
