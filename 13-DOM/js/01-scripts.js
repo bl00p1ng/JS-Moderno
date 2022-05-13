837a9f8f56388c383bc5171a178d34c94ef1a3ba
bl00p1ng <blooping@protonmail.com>
2022-05-13 18:56:20 -0500

    095 Acceder a elementos del HTML con document

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,21 @@
+let element;
+element = document;
+element = document.all;
+element = document.head;
+element = document.body;
+element = document.domain;  // Acceder al dominio actual
+element = document.forms;  // Acceder a los formularios del HTML
+
+// Un HTML Collection es similar a un array
+element = document.forms[0];
+element = document.forms[0].id;
+element = document.forms[0].method;
+element = document.forms[0].classList;
+element = document.forms[0].action;
+element = document.links;
+element = document.links[4].className;
+element = document.images;
+element = document.scripts;
+
+
+console.log(element);
