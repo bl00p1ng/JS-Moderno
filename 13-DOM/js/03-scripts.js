4fa0854dc3399117ab6e2cd0e358dcb90eeb8ec2
bl00p1ng <blooping@protonmail.com>
2022-05-13 19:18:34 -0500

    097 getElementById

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,7 @@
+// Acceder a un elemento por su ID
+const form = document.getElementById('formulario');
+console.log(form);
+
+// Si el ID no existe
+const dontExist = document.getElementById('dont-exist');
+console.log(dontExist);
