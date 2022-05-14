f6a80b68dd25177a94cdefbdbbe1f51575f9d853
bl00p1ng <blooping@protonmail.com>
2022-05-14 11:19:32 -0500

    098 querySelector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,18 @@
+const card = document.querySelector('.card');
+console.log(card);
+
+// Admite selectores especificos como en CSS
+const info = document.querySelector('.premium .info');
+console.log(info);
+
+// Seleccionar el segundo card de hospedaje
+const secondCard = document.querySelector('section.hospedaje .card:nth-child(2');
+console.log(secondCard);
+
+// Seleccionar ID
+const form = document.querySelector('#formulario');
+console.log(form);
+
+// Seleccionar tags
+const navigation = document.querySelector('nav');
+console.log(navigation);
