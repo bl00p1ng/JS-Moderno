5ce41247a4ed2e137c4c03db2c6e1f1b28977012
bl00p1ng <blooping@protonmail.com>
2022-05-14 11:25:04 -0500

    099 querySelectorAll

---
+++ 13-DOM/js/05-scripts.js
@@ -0,0 +1,6 @@
+const cards = document.querySelectorAll('.card');
+console.log(cards);
+
+// Si un elemento no existe
+const dontExist = document.querySelectorAll('.dont-exist');
+console.log(dontExist);
