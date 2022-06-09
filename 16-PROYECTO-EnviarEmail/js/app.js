c7b0bc2828b544fccf7765c244894b1601a3e0d6
bl00p1ng <blooping@protonmail.com>
2022-06-09 09:35:01 -0500

    124 Primeros Pasos con el Proyecto

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,12 @@
+// VARIABLES
+const sendBtn = document.querySelector('#enviar');
+
+// FUNCIONES
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', startApp);
+}
+
+function startApp() {
+    console.log('Iniciando...');
+}
