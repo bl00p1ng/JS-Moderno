f33615f901e3bc98d35801df9a02e68701fc8938
bl00p1ng <blooping@protonmail.com>
2022-06-09 14:30:02 -0500

    126 Limpiar los mensajes de error

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,5 +1,6 @@
 // VARIABLES
 const sendBtn = document.querySelector('#enviar');
+const form = document.querySelector('#enviar-mail');
 // Campos del form
 const email = document.querySelector('#email');
 const subject = document.querySelector('#asunto');
@@ -26,6 +27,23 @@ function validateForm(e) {
     if (e.target.value.length > 0) {
         console.log('Si hay algo');
     } else {
+        // Agregar bordes rojos al campo vacio
         e.target.classList.add('border', 'border-red-500');
+
+        // Mostrar un mensaje de error
+        showError();
+    }
+}
+
+// Mostrar un error en el form
+function showError() {
+    const errorMsg = document.createElement('p');
+    errorMsg.textContent = 'Todos los campos son obligatorios';
+    errorMsg.classList.add('error', 'border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center');
+    
+    const errors = document.querySelectorAll('.error');
+    
+    if (errors.length === 0) {
+        form.appendChild(errorMsg);
     }
 }
