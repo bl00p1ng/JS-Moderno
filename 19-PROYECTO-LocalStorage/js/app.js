aa28d198fb86acae0c357a39cf316b88203d9dc8
bl00p1ng <blooping@protonmail.com>
2022-06-10 13:39:34 -0500

    146 Validando el Formulario

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -14,5 +14,31 @@ function eventListeners() {
 function addTweet(e) {
     e.preventDefault();
 
-    console.log('Agregando tweet...');
+    // Textarea donde se escribe el tweet
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validar Tweet
+    if (tweet === '') {
+        showError('El Tweet no puede estar vacio');
+
+        return;  // Detiene ejecución de la función
+    }
+
+    console.log('Test');
+}
+
+// Mostrar un mensaje de error
+function showError(msg) {
+    const error = document.createElement('p');
+    error.textContent = msg;
+    error.classList.add('error');
+
+    // Insertar en el contenido
+    const content = document.querySelector('#contenido');
+    content.appendChild(error);
+
+    // Borrar mensaje de error después de 3 segundos
+    setTimeout(() => {
+        error.remove();
+    }, 3000);
 }
