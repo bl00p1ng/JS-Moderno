5710d13277ab963359dfda29083140922bf2b968
bl00p1ng <blooping@protonmail.com>
2022-06-10 10:47:10 -0500

    142 Obtener datos de LocalStorage

---
+++ 18-LocalStorage/js/02-app.js
@@ -0,0 +1,9 @@
+const name = localStorage.getItem('name');
+
+console.log(name);
+
+const productJSON = JSON.parse(localStorage.getItem('product'));
+console.log(productJSON);
+
+const months = JSON.parse(localStorage.getItem('months'));
+console.log(months);
