7c4e1c8ce43fe075110f253471240d066ba968a1
bl00p1ng <blooping@protonmail.com>
2022-06-09 21:18:26 -0500

    134 Leer el valor de la marca del automovil

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,10 +1,65 @@
 // Variables
 const result = document.querySelector('#resultado');
+const brand = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minPrice = document.querySelector('#minimo');
+const maxPrice = document.querySelector('#maximo');
+const doors = document.querySelector('#puertas');
+const transmission = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+const maxYear = new Date().getFullYear();
+const minYear = maxYear - 10;
+
+// Generar un objeto con la búsqueda
+const searchData = {
+    brand: '',
+    year: '',
+    minPrice: '',
+    maxPrice: '',
+    doors: '',
+    transmission: '',
+    color: '',
+};
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
+    // Muestra los autos al cargar
     showCars();
+
+    // LLenar las opciones de años
+    fillYearSelect();
 });
+
+// Event Listener de los select de búsqueda
+brand.addEventListener('change', e => {
+    searchData.brand = e.target.value;
+});
+
+year.addEventListener('change', e => {
+    searchData.year = e.target.value;
+});
+
+minPrice.addEventListener('change', e => {
+    searchData.minPrice = e.target.value;
+});
+
+maxPrice.addEventListener('change', e => {
+    searchData.maxPrice = e.target.value;
+});
+
+doors.addEventListener('change', e => {
+    searchData.doors = e.target.value;
+});
+
+transmission.addEventListener('change', e => {
+    searchData.transmission = e.target.value;
+});
+
+color.addEventListener('change', e => {
+    searchData.color = e.target.value;
+    console.table(searchData);
+});
+
 
 // Funciones
 function showCars() {
@@ -17,4 +72,13 @@ function showCars() {
 
         result.appendChild(carHTML);
     });
+}
+
+function fillYearSelect() {
+    for (let i = maxYear; i >= minYear; i--) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        year.appendChild(option);
+    }
 }
