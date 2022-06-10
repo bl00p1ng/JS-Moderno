856f9da1f04942af9fd611775840e8904610b08f
bl00p1ng <blooping@protonmail.com>
2022-06-10 16:18:09 -0500

    152 El Objeto Date()

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,10 @@
+const today = new Date();
+
+let value = today;
+value = today.getFullYear();
+value = today.getMonth();
+value = today.getMinutes();
+value = today.getHours();
+value = today.getTime();
+
+console.log(value);
