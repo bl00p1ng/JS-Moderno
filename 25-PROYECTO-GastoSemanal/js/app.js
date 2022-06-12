20c599b6413f8c665c41ed0d0cef957f55266991
bl00p1ng <blooping@protonmail.com>
2022-06-11 21:57:30 -0500

    170 Creando e instanciando la clase de Presupuesto

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -2,24 +2,47 @@
 const form = document.querySelector('#agregar-gasto');
 const listExpenses = document.querySelector('#gastos ul');
 
+/* Crea la variable de presupuesto en un contexto global.
+   más tarde se usará esta variable para instanciar Budget */
+let budget;
+
+
 // EVENT LISTENERS
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', askBudget);
 }
 
+
 // CLASES
+class Budget {
+    constructor(userBudget) {
+        this.budget = Number(userBudget);
+        this.remaining = Number(userBudget);
+        this.expenses = [];
+    }
+}
+
+class UI {
+
+}
+
+// Instanciar UI
+const ui = new UI();
 
 
 // FUNCIONES
 function askBudget() {
-    const budget = prompt('¿Cuál es tu presupuesto?');
-    console.log(budget);
+    const userBudget = prompt('¿Cuál es tu presupuesto?');
 
     /* Si el usuario no ingresa nada, cancela, no ingresa un número,
     o ingresa un número negativo, se recarga la pagina para abrir 
     el prompt otra vez */
-    if (budget === '' | budget === null | isNaN(budget) | budget <= 0) {
+    if (userBudget === '' | userBudget === null | isNaN(userBudget) | userBudget <= 0) {
         window.location.reload();
     }
+
+    // Instanciar presupuesto
+    budget = new Budget(userBudget);
+    console.log(budget);
 }
