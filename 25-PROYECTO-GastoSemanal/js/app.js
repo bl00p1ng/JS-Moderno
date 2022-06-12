4c7921c7e2f5edf2d94c88472c97bbfb814865d9
bl00p1ng <blooping@protonmail.com>
2022-06-11 22:32:13 -0500

    172 Validación del Formulario

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -2,15 +2,15 @@
 const form = document.querySelector('#agregar-gasto');
 const listExpenses = document.querySelector('#gastos ul');
 
-/* Crea la variable de presupuesto en un contexto global.
-   más tarde se usará esta variable para instanciar Budget */
-let budget;
-
 
 // EVENT LISTENERS
 eventListeners();
 function eventListeners() {
+    // Preguntar presupuesto una vez termine de cargar la página
     document.addEventListener('DOMContentLoaded', askBudget);
+
+    // Obtener los datos del formulario de gastos
+    form.addEventListener('submit', addExpense);
 }
 
 
@@ -23,6 +23,10 @@ class Budget {
     }
 }
 
+/* Crea la variable de presupuesto en un contexto global.
+   más tarde se usará esta variable para instanciar Budget */
+let budget;
+
 class UI {
     // Muestra el presupuesto
     showBudget(quantity) {
@@ -32,6 +36,29 @@ class UI {
         // Agregar al HTML
         document.querySelector('#total').textContent = budget;
         document.querySelector('#restante').textContent = remaining;
+    }
+    
+    // Mostrar mensaje de alerta
+    showAlert(msg, alertType) {
+        // Crear HTML
+        const pMsg = document.createElement('p');
+        pMsg.classList.add('text-center', 'alert');
+        pMsg.textContent = msg;
+
+        // Validar tipo de alerta
+        if (alertType === 'error') {
+            pMsg.classList.add('alert-danger');
+        } else {
+            pMsg.classList.add('alert-success');
+        }
+
+        // Insertar HTML
+        document.querySelector('.primario').insertBefore(pMsg, form);
+
+        // Retirar alerta después de 3 segundos
+        setTimeout(() => {
+            pMsg.remove();
+        }, 3000);
     }
 }
 
@@ -46,7 +73,7 @@ function askBudget() {
     /* Si el usuario no ingresa nada, cancela, no ingresa un número,
     o ingresa un número negativo, se recarga la pagina para abrir 
     el prompt otra vez */
-    if (userBudget === '' | userBudget === null | isNaN(userBudget) | userBudget <= 0) {
+    if (userBudget === '' || userBudget === null || isNaN(userBudget) || userBudget <= 0) {
         window.location.reload();
     }
 
@@ -55,4 +82,23 @@ function askBudget() {
 
     // Mostrar el presupuesto
     ui.showBudget(budget);
+}
+
+function addExpense(e) {
+    e.preventDefault();
+
+    // Leer datos del form
+    const expenseName = document.querySelector('#gasto').value;
+    const value = document.querySelector('#cantidad').value;
+
+    // Validar campos
+    if (expenseName === '' || value === '') {
+        ui.showAlert('Todos los campos son obligatorios', 'error');
+        return;
+    } else if (value <= 0 || isNaN(value)) {
+        ui.showAlert('Cantidad no valida', 'error');
+        return;
+    } else {
+        ui.showAlert('Correcto', 'succcess');
+    }
 }
