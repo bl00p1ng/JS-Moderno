1720d1f6b22dda3f1c337e2b22c7f8d3b2828f0e
bl00p1ng <blooping@protonmail.com>
2022-06-13 20:08:15 -0500

    180 Validación y Mostrar un mensaje de error

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -13,12 +13,16 @@ const appointmentContainer = document.querySelector('#citas');
 // EVENT LISTENERS
 eventListeners();
 function eventListeners() {
+    // Campos del formulario
     petInput.addEventListener('input', appointmentData);
     ownerInput.addEventListener('input', appointmentData);
     phoneInput.addEventListener('input', appointmentData);
     dateInput.addEventListener('input', appointmentData);
     hourInput.addEventListener('input', appointmentData);
     symptomsInput.addEventListener('input', appointmentData);
+
+    // Formulario
+    form.addEventListener('submit', newAppointment);
 }
 
 // Objeto con información de la cita
@@ -32,7 +36,46 @@ const appointmentObj = {
 };
 
 // CLASES
+class Appointments {
+    constructor() {
+        this.appointments = [];
+    }
+}
 
+class UI {
+    // Mostrar un mensaje de alerta
+    showAlert(msg, alertType) {
+        // Crear el div contenedor de la alerta
+        const alertDiv = document.createElement('div');
+        alertDiv.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        // Crear el párrafo con el mensaje de alerta
+        const alertMsg = document.createElement('p');
+        alertMsg.textContent = msg;
+        alertMsg.classList.add('font-weight-bold', 'mb-0');
+        alertDiv.appendChild(alertMsg);
+
+        // Validar tipo de alerta
+        if (alertType === 'error') {
+            alertDiv.classList.add('alert-danger');
+        } else {
+            alertDiv.classList.remove('alert-danger');
+            alertDiv.classList.add('alert-success');
+        }
+
+        // Agregar alerta al HTML
+        document.querySelector('#contenido').insertBefore(alertDiv, document.querySelector('.agregar-cita'));
+
+        // Eliminar mensaje de alerta despues de 3 segundos
+        setTimeout(() => {
+            alertDiv.remove();
+        }, 3000);
+    }
+}
+
+// Instanciar clases
+const appointment = new Appointments();
+const ui = new UI();
 
 // FUNCIONES
 // Añade datos al objeto de cita (appointmentObj)
@@ -40,4 +83,18 @@ function appointmentData(e) {
     // Usa name como key para asignarle valores a appointmentObj
     appointmentObj[e.target.name] = e.target.value;
     console.log(appointmentObj);
+}
+
+// Valida y crea una nueva cita
+function newAppointment(e) {
+    e.preventDefault();
+
+    // Extraer datos de appointmentObj
+    const {pet, owner, phone, date, hour, symptoms} = appointmentObj;
+
+    // Validar datos ingresados por el usuario
+    if (pet === '' || owner === '' || phone === '' || date === '' || hour === '' | symptoms === '') {
+        ui.showAlert('Todos los campos son obligatorios', 'error');
+        return;
+    }
 }
