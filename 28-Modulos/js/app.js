a65e24572e950c23c6693104d685bd022c795b99
bl00p1ng <blooping@protonmail.com>
2022-06-15 14:51:51 -0500

    195 Exportar e Importar Funciones

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,8 @@
-import {clientName, saving} from './cliente.js'
+import {clientName, saving, showInfo, hasSaving} from './cliente.js'
 
 console.log(clientName);
 console.log(saving);
+
+console.log(showInfo(clientName, saving));
+
+hasSaving(saving);
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -1,2 +1,14 @@
 export const clientName = 'Andrés';
 export const saving = 500;
+
+export function showInfo(name, saving) {
+    return `Cliente: ${name} - Ahorro: ${saving}`;
+}
+
+export function hasSaving(saving) {
+    if (saving > 0) {
+        console.log('Si tiene saldo');
+    } else {
+        console.log('No tiene saldo');
+    }
+}
