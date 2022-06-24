580a7d4ad6f113b78a069ed819a649c1f450b3b4
bl00p1ng <blooping@protonmail.com>
2022-06-23 22:37:26 -0500

    240 Primeros pasos de este Proyecto

---
+++ 37-PROYECTO-BuscarCanciones/js/app.js
@@ -0,0 +1,40 @@
+import * as UI from './ui.js'
+
+// ********** EVENT LISTENERS **********
+UI.form.addEventListener('submit', searchSong);
+
+// ********** FUNCIONES **********
+// Obtener y validar datos del form
+function searchSong(e) {
+    e.preventDefault();
+
+    // Obtener datos del form
+    const artist = document.querySelector('#artista').value;
+    const song = document.querySelector('#cancion').value;
+
+    // Validar datos del form
+    if (artist === '' || song === '') {
+        showError('Ambos campos son obligatorios');
+        return;
+    }
+}
+
+// Mostrar un mensaje de error en la interfaz
+function showError(msg) {
+    // Validar que solo se muestre una alerta a la vez
+    if (!document.querySelector('.error')) {
+        const alert = document.createElement('div');
+        alert.classList.add('error');
+        alert.innerHTML = `
+            <p><strong>${msg}</strong></p>
+        `;
+    
+        // Agregar al DOM
+        UI.messages.appendChild(alert);
+
+        // Eliminar alerta después de 3 segundos
+        setTimeout(() => {
+            alert.remove();
+        }, 3000);
+    }
+}
---
+++ 37-PROYECTO-BuscarCanciones/js/ui.js
@@ -0,0 +1,5 @@
+export const form = document.querySelector('#formulario-buscar');
+export const search = document.querySelector('#buscar');
+export const messages = document.querySelector('#mensajes');
+export const result = document.querySelector('#resultado');
+export const headingResult = document.querySelector('.letra-resultado h2');
