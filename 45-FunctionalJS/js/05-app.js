eda967dc9d368af0933e149710aff3786eb61f42
bl00p1ng <blooping@protonmail.com>
2022-06-30 22:30:26 -0500

    281 Menos cantidad de código en tus Funciones

--- 45-FunctionalJS/js/05-app.js
+++ 45-FunctionalJS/js/05-app.js
@@ -1,10 +1,22 @@
-const carrito = [
-    { nombre: 'Monitor 20 Pulgadas', precio: 500},
-    { nombre: 'Televisión 50 Pulgadas', precio: 700},
-    { nombre: 'Tablet', precio: 300},
-    { nombre: 'Audifonos', precio: 200},
-    { nombre: 'Teclado', precio: 50},
-    { nombre: 'Celular', precio: 500},
-    { nombre: 'Bocinas', precio: 300},
-    { nombre: 'Laptop', precio: 800},
+const cart = [
+    { name: 'Monitor 20 Pulgadas', price: 500},
+    { name: 'Televisión 50 Pulgadas', price: 700},
+    { name: 'Tablet', price: 300},
+    { name: 'Audifonos', price: 200},
+    { name: 'Teclado', price: 50},
+    { name: 'Celular', price: 500},
+    { name: 'Bocinas', price: 300},
+    { name: 'Laptop', price: 800},
 ];
+
+// En prog funcional, por lo general se busca acortar el código
+const getNames = p => p.name;
+
+const greaterThan400 = p => p.price > 400;
+
+// A diferencia de forEach, map retorna un array nuevo sin alterar el original
+const result = cart.map(getNames);
+console.log(result);
+
+const result2 = cart.filter(greaterThan400);
+console.log(result2);
