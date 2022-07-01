1e68519133a9304167b6ec360601821c8e23e72f
bl00p1ng <blooping@protonmail.com>
2022-06-30 22:26:05 -0500

    280 .map es muy utilizado en Functional JS

--- 45-FunctionalJS/js/04-app.js
+++ 45-FunctionalJS/js/04-app.js
@@ -1,10 +1,16 @@
-const carrito = [
-    { nombre: 'Monitor 20 Pulgadas', precio: 500},
-    { nombre: 'Televisión 50 Pulgadas', precio: 700},
-    { nombre: 'Tablet', precio: 300},
-    { nombre: 'Audifonos', precio: 200},
-    { nombre: 'Teclado', precio: 50},
-    { nombre: 'Celular', precio: 500},
-    { nombre: 'Bocinas', precio: 300},
-    { nombre: 'Laptop', precio: 800},
+const cart = [
+    { name: 'Monitor 20 Pulgadas', price: 500},
+    { name: 'Televisión 50 Pulgadas', price: 700},
+    { name: 'Tablet', price: 300},
+    { name: 'Audifonos', price: 200},
+    { name: 'Teclado', price: 50},
+    { name: 'Celular', price: 500},
+    { name: 'Bocinas', price: 300},
+    { name: 'Laptop', price: 800},
 ];
+
+const getNames = product => product.name;
+
+// A diferencia de forEach, map retorna un array nuevo sin alterar el original
+const result = cart.map(getNames);
+console.log(result);
