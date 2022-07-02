fcaf9ac9a55b52c6b267b15cd77552f18bfde5db
bl00p1ng <blooping@protonmail.com>
2022-07-01 21:47:43 -0500

    289 Coercion

---
+++ 46-DominandoJavaScript/js/03-app.js
@@ -0,0 +1,8 @@
+// Coercion
+// --> Implicita
+const number1 = 20;
+const number2 = '40';
+console.log(number1 + number2);  // La salida es -> '2040'
+
+// --> Explícita
+console.log(Number(number2));
