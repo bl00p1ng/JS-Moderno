e29a5bb7f21f30443a756dd404b4e0045cc4a3e5
bl00p1ng <blooping@protonmail.com>
2022-07-03 19:57:47 -0500

    299 Hacer una PWA Instalable

--- 47-ServiceWorkers-PWA/serviceWorker.js
+++ 47-ServiceWorkers-PWA/serviceWorker.js
@@ -9,3 +9,8 @@ self.addEventListener('activate', e => {
     console.log('Service worker activado');
     console.log(e);
 });
+
+// Evento fetch para registrar archivos estáticos
+self.addEventListener('fetch', e => {
+    console.log('Fetch...', e);
+});
