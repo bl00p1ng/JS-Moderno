64b236a71c19b454a7de4f172816340ccf5335ec
bl00p1ng <blooping@protonmail.com>
2022-07-03 21:53:09 -0500

    300 Cachear Archivos

--- 47-ServiceWorkers-PWA/serviceWorker.js
+++ 47-ServiceWorkers-PWA/serviceWorker.js
@@ -1,7 +1,28 @@
+// Nombre del cache
+const cacheName = 'apv-v1';
+// Archivos que se van a cachear para cuando la App este offline
+const archives = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/apv.js'
+];
+
 // Cuando se instala el service worker
 self.addEventListener('install', e => {
     console.log('Se instalo el service worker');
-    console.log(e);
+    
+    // Esperar hasta que se terminen de descargar los archivos de cache
+    e.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                console.log('Guardando en cache...');
+                // Añadir archivos a la cache
+                cache.addAll(archives);
+            })
+    );
 });
 
 // Cuando se activa el service worker
