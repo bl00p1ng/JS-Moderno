7264b44fa049c7335533380aec8178eab8f0ecd2
bl00p1ng <blooping@protonmail.com>
2022-07-04 20:28:55 -0500

    305 Class Pattern

--- 48-DesignPatterns/js/01-app.js
+++ 48-DesignPatterns/js/01-app.js
@@ -1,1 +1,10 @@
-console.log('Holi');
+// Class pattern
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+}
+
+const person = new Person('Andrés', 'andres@mail.com');
+console.log(person);
