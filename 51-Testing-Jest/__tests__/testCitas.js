e30dbf02da9a55d60783b19cf001acc3b7116ff9
bl00p1ng <blooping@protonmail.com>
2022-07-06 15:38:17 -0500

    330 Testing al Proyecto de Citas - Actualizar y Eliminar

--- 51-Testing-Jest/__tests__/testCitas.js
+++ 51-Testing-Jest/__tests__/testCitas.js
@@ -1,10 +1,13 @@
 import Citas from '../js/classes/Citas.js';
 
 describe('Probar la clase de Citas', () => {
+    const id = Date.now();
+    const citas = new Citas();
+
     test('Agregar una nueva cita', () => {
-        const citas = new Citas();
 
         const citaObj = {
+            id,
             mascota: 'Luna',
             propietario: 'Andrés',
             telefono: '4618923',
@@ -13,10 +16,32 @@ describe('Probar la clase de Citas', () => {
             sintomas: 'No come'
         }
 
-        citaObj.id = Date.now();
-
         citas.agregarCita(citaObj);
 
+        expect(citas).toMatchSnapshot();
+    });
+
+    test('Actualizar cita', () => {
+        const citaActualizada = {
+            id,
+            mascota: 'Lunita',
+            propietario: 'Andrés López',
+            telefono: '4618923',
+            fecha: '22-07-2022',
+            hora:'10:30',
+            sintomas: 'No come'
+        }
+
+        citas.editarCita(citaActualizada);
+
+        // Testear
+        expect(citas).toMatchSnapshot();
+    });
+
+    test('Eliminar cita', () => {
+        citas.eliminarCita(id);
+
+        // Testing
         expect(citas).toMatchSnapshot();
     });
 });
