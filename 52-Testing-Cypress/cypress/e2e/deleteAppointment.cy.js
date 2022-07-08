644d94b0670c445cd54a959c0fd555e45ab553a4
bl00p1ng <blooping@protonmail.com>
2022-07-08 16:04:09 -0500

    337 Eliminar Citas

---
+++ 52-Testing-Cypress/cypress/e2e/deleteAppointment.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+describe('Llena los campos para crear una nueva cita y la elimina', () => {
+    it('Campos para crear una nueva cita', () => {
+        // Abrir el proyecto
+        cy.visit('index.html');
+
+        // Llenar campos del form
+        cy.get('[data-cy="pet-input"]')
+            .type('Luna');
+        cy.get('[data-cy="owner-input"]')
+            .type('Andrés');
+        cy.get('[data-cy="phone-input"]')
+            .type('4567816363');
+        cy.get('[data-cy="date-input"]')
+            .type('2022-07-12');
+        cy.get('[data-cy="hour-input"]')
+            .type('10:30');
+        cy.get('[data-cy="symptoms-textarea"]')
+            .type('No come bien');
+
+        // Enviar form
+        cy.get('[data-cy="submit-appointment"]')
+            .click();
+
+        // Verificar heading de las citas
+        cy.get('[data-cy="appointments-heading"]')
+            .invoke('text')
+            .should('equal', 'Administra tus Citas');
+
+        // Verificar la alerta de cita creada
+        cy.get('[data-cy="alert"]')
+            // Revisar que tenga la clase correcta
+            .should('have.class', 'alert-success')
+            .invoke('text')  // Obtener el texto del elemento
+            .should('equal', 'Se agregó correctamente');
+    });
+
+    it('Eliminar una cita', () => {
+        cy.get('[data-cy="delete-btn"]')
+            .click();
+
+        // Verificar heading de las citas
+        cy.get('[data-cy="appointments-heading"]')
+            .invoke('text')
+            .should('equal', 'No hay Citas, comienza creando una');
+    });
+});
