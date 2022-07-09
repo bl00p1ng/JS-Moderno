5e6ca2d5ac2783415b627fce484475e90ef986f7
bl00p1ng <blooping@protonmail.com>
2022-07-08 19:48:42 -0500

    343 Creando un Servidor con webpack dev server

--- 53-Webpack-ESLint/webpack.config.js
+++ 53-Webpack-ESLint/webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/app.js',  // Punto de entrada de la App
+    entry: {
+        index: './js/app.js',
+    },  // Punto de entrada de la App
     output: {
         path: path.resolve(__dirname, 'public/js'),
         filename: 'bundle.js'
     },
     mode: 'development',
+    devServer: {
+        static: path.join(__dirname, '/'),
+        compress: true,
+    },
     module:{
         rules: [
             {
