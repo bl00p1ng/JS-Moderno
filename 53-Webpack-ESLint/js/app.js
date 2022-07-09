7cecb3e44d4ceb921c24be19254b9d31744c713d
bl00p1ng <blooping@protonmail.com>
2022-07-08 21:38:04 -0500

    345 Añadiendo ESLint a nuestro código

--- 53-Webpack-ESLint/js/app.js
+++ 53-Webpack-ESLint/js/app.js
@@ -2,8 +2,3 @@ import App from './classes/App.js';
 
 // eslint-disable-nextline
 const app = new App();
-
-console.log('Holi');
-console.log('Que hace');
-console.log('test 2');
-console.log('test 3');
